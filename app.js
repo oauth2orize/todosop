34ad752671896bde80324390baa16e175806fc93
Jared Hanson <jaredhanson@gmail.com>
2022-04-25 12:56:00 -0700

    Update app and db.

--- app.js
+++ app.js
@@ -1,60 +1,65 @@
+var createError = require('http-errors');
 var express = require('express');
-var passport = require('passport');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var passport = require('passport');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
+// pass the session to the connect sqlite3 module
+// allowing it to inherit from session.Store
+var SQLiteStore = require('connect-sqlite3')(session);
+
 var authRouter = require('./routes/auth');
-var consentRouter = require('./routes/consent');
-var userinfoRouter = require('./routes/userinfo');
+var authzRouter = require('./routes/authz');
 var oauth2Router = require('./routes/oauth2');
-var myaccountRouter = require('./routes/myaccount');
-var usersRouter = require('./routes/users');
+var userinfoRouter = require('./routes/userinfo');
 
 var app = express();
-
-require('./boot/db')();
-require('./boot/auth')();
-require('./boot/oauth2')();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// Use application-level middleware for common functionality, including
-// logging, parsing, and session handling.
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(require('express-session')({ name: 'as.sid', secret: 'keyboard cat', resave: false, saveUninitialized: false }));
-
+app.use(session({
+  name: 'id.sid',
+  secret: 'keyboard cat',
+  resave: false, // don't save session if unmodified
+  saveUninitialized: false, // don't create session until something stored
+  store: new SQLiteStore({ db: 'sessions.db', dir: './var/db' })
+}));
 app.use(function(req, res, next) {
   console.log('# ' + req.method + ' ' + req.url)
   console.log(req.headers)
   console.log(req.session)
   next();
 });
-
-app.use(function(req, res, next) {
-  var msgs = req.session.messages || [];
-  res.locals.messages = msgs;
-  res.locals.hasMessages = !! msgs.length;
-  req.session.messages = [];
-  next();
-});
-app.use(passport.initialize());
 app.use(passport.authenticate('session'));
 
-// Define routes.
-app.use('/', indexRouter);
 app.use('/', authRouter);
-app.use('/consent', consentRouter);
+app.use('/', authzRouter);
 app.use('/oauth2', oauth2Router);
-app.use('/myaccount', myaccountRouter);
-app.use('/users', usersRouter);
-app.use('/userinfo', userinfoRouter);
+app.use('/openidconnect', userinfoRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
 module.exports = app;
